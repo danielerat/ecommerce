f0e913723e88886070a558fdf068ad52d4b3d84f
danielerat <davidodo2015@gmail.com>
2023-02-11 17:55:17 +0200

    Final Commit for admin section

--- tags/models.py
+++ tags/models.py
@@ -1,20 +1,24 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class TaggedItemManager(models.Manager):
-    def get_tags_for(self, object_type, object_id):
-        # Find the content type id for the product model
-        content_type = ContentType.objects.get_for_model(object_type)
-        return TaggedItem.objects.select_related('tag').filter(content_type=content_type, object_id=object_id)
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
 
 
-# Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.label
 
 
@@ -24,6 +28,3 @@ class TaggedItem(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-
-    def __str__(self):
-        return self.tag.label
