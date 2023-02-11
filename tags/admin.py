5cbc460d2574c9c986effdc182ebbdccf8944cdc
danielerat <davidodo2015@gmail.com>
2023-02-11 19:19:32 +0200

    Installing Django Rest Framework

--- tags/admin.py
+++ tags/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag
+from .models import Tag,TaggedItem
 
 # Register your models here.
 
@@ -7,3 +7,7 @@ from .models import Tag
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     search_fields = ['label']
+
+@admin.register(TaggedItem)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag','content_type','object_id','content_object']
--- tags/models.py
+++ tags/models.py
@@ -17,7 +17,6 @@ class TaggedItemManager(models.Manager):
 
 class Tag(models.Model):
     label = models.CharField(max_length=255)
-
     def __str__(self) -> str:
         return self.label
 
