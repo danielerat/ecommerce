c5f8fbf58329e833527fe220d97cc7a79d800869
danielerat <davidodo2015@gmail.com>
2023-02-17 20:26:11 +0200

    Fine Tuning the custom user model from our core app

--- core/admin.py
+++ core/admin.py
@@ -8,6 +8,23 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name","phone_number", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
     add_fieldsets = (
         (
             None,
