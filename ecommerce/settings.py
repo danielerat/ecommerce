44d703f635f220a26b757ed875e6a025e2cd6378
danielerat <davidodo2015@gmail.com>
2023-02-18 23:00:37 +0200

    Creating the system to upload image apply validation and install django cors to allow request from different urls

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,6 +71,10 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 ROOT_URLCONF = 'ecommerce.urls'
 
